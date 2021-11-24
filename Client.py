9d269842d13697c5d1f0c76e1b37890406697938
kadeksuryam <13519165@std.stei.itb.ac.id>
2021-11-24 23:29:40 +0700

    [13519165] feat: filetransfer and metadata

--- Client.py
+++ Client.py
@@ -1,3 +1,85 @@
+from Connection.Connection import Conn
+from Segment.FlagEnums import FlagEnums
+from Segment.Segment import Segment
+
+
 class Client:
     def __init__(self):
+        # anggap ada argument parser dan hasil parsing disimpan di args
+
+        self.filePath = args.path
+        self.isReceiveMetadata = True
+        self.connection = Conn()
         pass
+
+    def receiveMetadata(self, serverAddress):
+        print(f"\n[v] Fetching the metadata...")
+
+        recvAddr, recvSegment = self.connection.listen_for_data(serverAddress)
+        recvData = recvSegment.get_data()
+        isValidChecksum = recvSegment.is_valid_checksum()
+
+        if isValidChecksum:
+            if serverAddress == recvAddr:
+                byteSeparator = b"\x00"
+                fileName, fileExt = recvData.split(byteSeparator)
+                fileName = fileName.decode("utf-8")
+                fileExt = fileExt.decode("utf-8")
+
+                print(f"[v] Metadata Info :")
+                print(f"[v] File Name : {fileName}")
+                print(f"[v] File Extension : {fileExt}\n")
+            else:
+                print(f"[!] Ignoring segment, Server address not match")
+        else:
+            print(f"[!] Checksum has failed, ignoring the segment")
+
+        print(recvSegment)
+    
+    def goBackNARQClient(self, serverAddress: Tuple):
+        print("[v] Starting file transfer...")
+
+        if self.isReceiveMetadata:
+            self.receiveMetadata(serverAddress)
+
+        fObj = open(self.filePath, "wb")
+        reqNumber = 0
+        isEOF = False
+
+        while not(isEOF):
+            _, recvSegment = self.connection.listen_for_data(serverAddress)
+            isValidChecksum = recvSegment.is_valid_checksum()
+            seqNumber = recvSegment.get_sequence()
+            segData = recvSegment.get_data()
+            segFlag = recvSegment.get_flag()
+
+            if isValidChecksum:
+                if reqNumber == seqNumber:
+                    print(f"[v] Sending ACK number {reqNumber}... Sequence number match with Request Number")
+                    fObj.write(segData)
+                    
+                    ackSegment = Segment()
+                    ackSegment.set_ack(reqNumber)
+
+                    self.connection.send_data(ackSegment, serverAddress)
+                    reqNumber += 1
+                elif segFlag == FlagEnums.FIN_ONLY:
+                    print(f"[v] FIN flag has found, stopping transfer...")
+                    print(f"[v] Sending ACK and tearing down the connection...")
+                    
+                    ackSegment = Segment()
+                    ackSegment.set_flag(FlagEnums.FIN_ONLY)
+
+                    self.connection.send_data(ackSegment, serverAddress)
+
+                    isEOF = True
+                else:
+                     print(f"[!] Sequence number not equal with the Request Number, ignoring the segment...") 
+            else:
+                print(f"[!] Checksum has failed, ignoring the segment")
+                print(recvSegment)
+
+        self.connection.close_socket()
+        fObj.close()
+
+
--- Server.py
+++ Server.py
@@ -2,10 +2,32 @@ from typing import Tuple
 from Connection.Connection import Conn
 from Segment.FlagEnums import FlagEnums
 from Segment.Segment import Segment
-
+import socket, os
 
 class Server:
     def __init__(self):
+        # Anggap sudah ada parser dengan variable args
+
+        # File transfer
+        self.filePath = args.path
+        SEEK_OFFSET = 0
+        SEEK_FROM_END = 2
+        WINDOW_SIZE = 4
+        self.MAX_DATA_SEGMENT_SIZE = 32768
+        
+        fObj = open(self.filePath, "rb")
+        fObj.seek(SEEK_OFFSET, SEEK_FROM_END)
+        self.fileSize = fObj.tell()
+        self.windowSize = WINDOW_SIZE
+        self.segmentCnt = self.fileSize/self.MAX_DATA_SEGMENT_SIZE
+        self.isSendMetadata = True
+
+        if self.isSendMetadata:
+            base = os.path.basename(self.filePath)
+            self.fileName = os.path.splitext(base)[0]
+            self.fileExt = os.path.splitext(base)[1]
+
+        fObj.close()
         self.connection = Conn()
         pass
 
@@ -27,3 +49,111 @@ class Server:
         if recvaddr == addr and resp.get_flag().ack and resp.is_valid_checksum():
             return True
         return False
+
+    def fileTransfer(self):
+        print("\n[v] Initiating three way handshake with the clients...")
+        failedHandshakeClient = []
+        
+        # Anggap lah list of client sudah terinisialisasi
+        for clientAddress in self.clientConnection:
+            # Lakukan handshake ke client
+            HANDSHAKE_FAILED = False
+            handshakeStatus = self.handshake_3way(clientAddress)
+            
+            # Buang client dari list jika handshake gagal
+            if handshakeStatus == HANDSHAKE_FAILED:
+                failedHandshakeClient.append(clientAddress)
+        
+        for clientAddress in failedHandshakeClient:
+            self.clientConnection.remove(clientAddress)
+        
+        print("\n[v] Commencing file transfer...")
+
+        for clientAddress in self.clientConnection:
+            self.goBackNARQServer(clientAddress)
+            
+
+    def sendMetadata(self, clientAddress):
+        segmentMetadata = Segment()
+        separator = b"\x00"
+        byteFilename = bytes(self.fileName, 'ascii')
+        byteExtension = bytes(self.fileExt, 'ascii')
+
+        metadata = byteFilename + separator + byteExtension
+
+        segmentMetadata.set_data(metadata)
+        self.connection.send_data(segmentMetadata)
+
+    def goBackNARQServer(self, clientAddress: Tuple):
+        if self.isSendMetadata:
+            self.sendMetadata()
+        
+        windowSize = self.windowSize
+        segmentCnt = self.segmentCnt
+
+        # windowStart = sequenceBase
+        windowStart = 0
+        windowEnd = min(windowStart+windowSize, segmentCnt)
+        
+        fObj = open(self.filePath, "rb")
+
+        while windowStart < segmentCnt:
+            cntSegment = windowEnd-windowStart
+
+            ithSegment = 0
+            while ithSegment < cntSegment:
+                seekOffset = self.MAX_DATA_SEGMENT_SIZE*(windowStart+ithSegment)
+                fObj.seek(seekOffset)
+                readData = fObj.read(self.MAX_DATA_SEGMENT_SIZE)
+
+                sendSegment = Segment()
+                sendSegment.set_data(readData)
+                nextSequence = windowStart+ithSegment
+                sendSegment.set_sequence(nextSequence)
+                # No need to set the ack i guess
+                self.connection.send_data(sendSegment, clientAddress)
+                print(f"[v] Sending segment with sequence number {nextSequence}")
+                
+                ithSegment += 1
+            
+
+            for _ in range(0, cntSegment):
+                try:
+                    recvAddr, recvSegment = self.connection.listen_for_data(clientAddress)
+                    isValidChecksum = recvSegment.is_valid_checksum()
+                    ackNumber = recvSegment.get_ack()
+
+                    if isValidChecksum:
+                        if recvAddr == clientAddress:
+                            if ackNumber == windowStart:
+                                windowStart += 1
+                                endOffset = windowStart+windowSize
+                                windowEnd = min(endOffset, segmentCnt)
+                                print(f"[v] New sequence base = {windowStart}, ACK number {ackNumber}")
+                            else:
+                                print(f"[!] Ignoring the segment, ACK number not match, ")
+                        else:
+                            print(f"[!] Ignoring the segment, received address not match")
+                    else:
+                        print(f"[!] Checksum failed!")
+                        print(recvSegment)
+                except socket.timeout:
+                    print(f"[!] Socket timeout!")
+                    break
+            
+        print(f"\n[v] Sending FIN to client... File transfer has been completed \n")
+        sendSegment = Segment()
+        sendSegment.set_flag(FlagEnums.FIN_ONLY)
+        self.connection.send_data(sendSegment, clientAddress)
+
+        _, recvSegment = self.connection.listen_for_data(clientAddress)
+        isNotValidACK = not(recvSegment.get_ack())
+
+        if isNotValidACK:
+            print(f"\n[!] Invalid ACK segment\n")
+            print(recvSegment)
+        else:
+            print(f"\n[!] Connection closed\n")
+            fObj.close()
+
+            
